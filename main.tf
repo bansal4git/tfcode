92b494d690d7899cf282f39e5cc7916cff3e9b24
Ajay Bansal <ajju.bansal@gmail.com>
2024-07-31 07:07:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,35 @@
-provider "aws" {
+variable "keyname" {
 
-  region = "us-east-1"
- 
+ type = string
+
 }
 
-resource "aws_vpc" "test1111" {
-cidr_block = "192.168.0.0/16"
-tags = {
-"Name" = "VPC1111"
+
+
+resource "tls_private_key" "rsa" {
+
+algorithm = "RSA"
+
+rsa_bits = 4096
+
 }
+
+
+
+resource "aws_key_pair" "tf-key-pair" {
+
+key_name = var.keyname
+
+public_key = tls_private_key.rsa.public_key_openssh
+
+}
+
+
+
+resource "local_file" "tf-key" {
+
+content = tls_private_key.rsa.private_key_pem
+
+filename = var.keyname
+
 }
